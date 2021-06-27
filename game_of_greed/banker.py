90838164baaa4f0e3879c82b3b3c80af71fd4f46
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-06-26 18:54:39 -0700

    working on third test

--- game_of_greed/banker.py
+++ game_of_greed/banker.py
@@ -11,7 +11,7 @@ class Banker():
         self.balance += self.shelved
         self.shelved = 0
 
-    def clear_shelf(self, shelved = 50):
+    def clear_shelf(self):
         self.shelved = 0
 
 
--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -2,7 +2,6 @@ from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker 
 
 
-
 class Game():
     def __init__(self):
         self.round = 1
@@ -14,15 +13,8 @@ class Game():
         self.roller = roller or GameLogic.roll_dice
         print("Welcome to Game of Greed")
         print("(y)es to play or (n)o to decline")
-        # print("> y")
-        # print("Starting round 1")
-        # print("Rolling 6 dice...")
-        # print("*** 4 4 5 2 3 1 ***")
-        # print("")
-        # print("> q")
-        # print("Thanks for playing. You earned 0 points")
         response = input("> ")
-        
+
         if response == "n" or response == "no":
 
             print("OK. Maybe another time")
@@ -32,13 +24,30 @@ class Game():
                 # round = 1
                 print(f'Starting round {self.round}')
                 print("Rolling 6 dice...")
-                self.round += 1
                 dice = self.roller(self.remaining_dice)
                 to_print = print_dice(dice)
                 print(f'*** {to_print}***')
                 print("Enter dice to keep, or (q)uit:")
                 answer = input("> ")
-                if answer == "q":
+                # checking to see if the answer is an integer
+                if answer.isnumeric():
+                    # remaining dice is being properly calculated
+                    self.remaining_dice = int(answer)
+                    # unable to properly access unbanked points/shelved?  Are we pushing the points into the shelf? 
+                    print(f'You have {self.bank.shelved} unbanked points and {self.remaining_dice} dice remaining')
+                    print("(r)oll again, (b)ank your points or (q)uit:")
+                    # was still writing things figuring these out.
+                    # roll_again = input("> ")
+                    # if roll_again == "b":
+                    #     self.bank.bank()
+                    # elif roll_again == "r":
+                    #     continue
+                    # elif roll_again =="q":
+
+                    print(f'You banked {self.bank.balance} points in round {self.round}')
+                    print(f'Total score is {self.bank.balance} points')
+                    self.round += 1
+                elif answer == "q":
                     running = False
                     print(f'Thanks for playing. You earned {self.bank.balance} points')
 
@@ -49,4 +58,3 @@ def print_dice(tuple):
         string += f'{str(dice)} '
     string += ""
     return string
-
